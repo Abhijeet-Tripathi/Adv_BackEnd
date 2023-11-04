e0389d71474733b0cd0d0e52eed5d448d2f27c7b
Abhijeet Tripathi <abhijeettripathia@gmail.com>
2023-11-04 15:51:24 +0530

    Added Router

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port=8000;
 
 const app=express();
 
+//to connect router
+app.use('/',require('./routes/index'));
+
+
 app.listen(port,function(err){
     if(err){
         console.log("Erore in running server");
